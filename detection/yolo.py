a7b49229fb3aa9c593c6696eb482c701c19d9dec
Gabe3L <gabe3lynch@gmail.com>
2025-08-16 03:55:40 -0400

    Added Presentation and .gitignore

---
+++ detection/yolo.py
@@ -0,0 +1,37 @@
+from typing import Tuple, List
+
+import torch
+import numpy as np
+from ultralytics import YOLO
+
+###############################################################
+
+CONFIDENCE_THRESHOLD: float = 0.6
+
+class YOLODetector:
+    def __init__(self) -> None:
+        self.device: torch.device = torch.device(
+            'cuda' if torch.cuda.is_available() else 'cpu')
+        self.model: YOLO = YOLO("train\\weights\\best.engine" if torch.cuda.is_available(
+        ) else "train\\weights\\best.onnx", task='detect')
+
+    def detect(self, frame: np.ndarray) -> Tuple[np.ndarray, np.ndarray, np.ndarray]:
+        with torch.no_grad():
+            results = self.model.predict(frame)[0]
+        return self.extract_detections(results)
+
+    def extract_detections(self, results) -> Tuple[np.ndarray, np.ndarray, np.ndarray]:
+        boxes: List[List[int]] = []
+        confidences: List[float] = []
+        class_ids: List[int] = []
+
+        for box in results.boxes:
+            x1, y1, x2, y2 = map(int, box.xyxy[0].cpu().numpy())
+            confidence: float = float(box.conf[0])
+
+            if confidence >= CONFIDENCE_THRESHOLD:
+                boxes.append([x1, y1, x2, y2])
+                confidences.append(confidence)
+                class_ids.append(int(box.cls[0]))
+
+        return np.array(boxes), np.array(confidences), np.array(class_ids)
